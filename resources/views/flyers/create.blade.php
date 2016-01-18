0bbef6725acd718357daa1e590f1c6eb33a3183e
Sonford <sonfordonyango@gmail.com>
2016-01-18 15:14:52 +0300

    The Flyer Form

--- resources/views/flyers/create.blade.php
+++ resources/views/flyers/create.blade.php
@@ -4,5 +4,8 @@
   <h1>Selling Your Home?</h1>
 
 
+        <form method="POST" ACTION="/flyers" enctype="multipart/form-data">
 
+          @include('flyers.form')
+        </form>
 @stop
---
+++ resources/views/flyers/form.blade.php
@@ -0,0 +1,48 @@
+@inject('countries', 'App\Http\Utilities\Country')
+<div class="form-group">
+    <label for="street">Street:</label>
+    <input type="text" name="name" id="name" class="form-control" value="{{ old('name') }}">
+</div>
+<div class="form-group">
+    <label for="city">City:</label>
+    <input type="text" name="city" id="city" class="form-control" value="{{ old('city') }}">
+</div>
+
+<div class="form-group">
+    <label for="zip"> Zip/ Podtal Code: </label>
+    <input type="text" name="zip" id="zip" class="form-control" value="{{ old('zip') }}">
+</div>
+<div class="form-group">
+    <label for="county">Country:</label>
+    <select name="country" id="country" class="form-control">
+
+        @foreach($countries::all() as $name => $code)
+            <option value="{{ $code }}"> {{ $name }}</option>
+
+        @endforeach
+    </select>
+</div>
+
+<div class="form-group">
+    <label for="state">State:</label>
+    <input type="text" name="state" id="state" class="form-control" value="{{ old('state ') }}">
+</div>
+<div class="form-group">
+    <label for="price">Sale Price :</label>
+    <input type="text" name="price" id="price" class="form-control" value="{{ old('price') }}">
+</div>
+<div class="form-group">
+    <label for="description">Home Description :</label>
+                    <textarea type="text" name="description" id="description" class="form-control" rows="10">
+
+                        {{ old('description') }}
+                    </textarea>
+</div>
+<div class="form-group">
+    <label for="photos"> Photos :</label>
+    <input type="file" name="photos" id="photos" class="form-control" value="{{ old('photos') }}">
+</div>
+
+<div class="form-group">
+    <button type="submit" class="btn btn-primary">Create Flyer</button>
+</div>
